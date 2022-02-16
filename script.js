36dad8fb50234cd0cac4091e0517db65aaecb85a
Fran Ribeiro <ribeirofrv@gmail.com>
2022-02-16 19:32:40 -0300

    Cumpre Requisito 8 - Seleciona apenas um item da lista

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 const criarTarefa = document.querySelector('#criar-tarefa');
 const listaTarefa = document.querySelector('#lista-tarefas');
 const textoTarefa = document.querySelector('#texto-tarefa');
-const itemDaLista = document.querySelectorAll('.tarefa');
 
 criarTarefa.addEventListener('click', () => {
   const adicionaTarefa = document.createElement('li');
-
+  
   if (textoTarefa.value === '') {
     alert('Campo vazio, adicione uma tarefa!');
   } else {
@@ -17,5 +16,11 @@ criarTarefa.addEventListener('click', () => {
 });
 
 listaTarefa.addEventListener('click', (event) => {
+  const itemDaLista = document.querySelectorAll('.tarefa');
+
+  for (let item = 0; item < itemDaLista.length; item += 1) {
+    itemDaLista[item].style.background = 'white';
+  }
+
   event.target.style.background = 'gray';
 });
