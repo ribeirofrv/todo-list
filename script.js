1b88944efe6a60eb8c5509b852a18e93c5a2f0f5
Fran Ribeiro <ribeirofrv@gmail.com>
2022-02-16 21:44:25 -0300

    Cumpre Requisito 10 - Adiciona botão para apagar toda as tarefas

--- script.js
+++ script.js
@@ -1,30 +1,47 @@
 const criarTarefa = document.querySelector('#criar-tarefa');
-const listaTarefa = document.querySelector('#lista-tarefas');
+const listaTarefas = document.querySelector('#lista-tarefas');
 const textoTarefa = document.querySelector('#texto-tarefa');
+const apagarLista = document.querySelector('#apaga-tudo');
 
-criarTarefa.addEventListener('click', () => {
-  const adicionaTarefa = document.createElement('li');
-  
-  if (textoTarefa.value === '') {
-    alert('Campo vazio, adicione uma tarefa!');
-  } else {
-    adicionaTarefa.innerText = textoTarefa.value;
-    listaTarefa.appendChild(adicionaTarefa);
-    adicionaTarefa.classList.add('tarefa');
-    textoTarefa.value = '';
-  }
-});
+const adicionaListaDeTarefa = () => {
+  criarTarefa.addEventListener('click', () => {
+    const adicionaTarefa = document.createElement('li');
 
-listaTarefa.addEventListener('click', (event) => {
-  const itemDaLista = document.querySelectorAll('.tarefa');
+    if (textoTarefa.value === '') {
+      alert('Campo vazio, adicione uma tarefa!');
+    } else {
+      adicionaTarefa.innerText = textoTarefa.value;
+      listaTarefas.appendChild(adicionaTarefa);
+      adicionaTarefa.classList.add('tarefa');
+      textoTarefa.value = '';
+    }
+  });
+};
 
-  for (let item = 0; item < itemDaLista.length; item += 1) {
-    itemDaLista[item].style.background = 'white';
-  }
+const marcaItensDaLista = () => {
+  listaTarefas.addEventListener('click', (event) => {
+    const itemDaLista = document.querySelectorAll('.tarefa');
 
-  event.target.style.background = 'gray';
-});
+    for (let item = 0; item < itemDaLista.length; item += 1) {
+      itemDaLista[item].style.background = 'white';
+    }
 
-listaTarefa.addEventListener('dblclick', (event) => {
-  event.target.classList.toggle('completed');
-});
+    event.target.style.background = 'gray';
+  });
+
+  listaTarefas.addEventListener('dblclick', (event) => {
+    event.target.classList.toggle('completed');
+  });
+};
+
+const apagaListaDeTarefas = () => {
+  apagarLista.addEventListener('click', () => {
+    listaTarefas.innerHTML = '';
+  });
+};
+
+window.onload = () => {
+  adicionaListaDeTarefa();
+  marcaItensDaLista();
+  apagaListaDeTarefas();
+};
