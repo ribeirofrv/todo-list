5a7243261b86ce59fd7dff41d4dfe58439224c8c
Fran Ribeiro <ribeirofrv@gmail.com>
2022-02-22 00:11:23 -0300

    Cumpre Requisito 11 - Adiciona botao para remover itens finalizados

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ const criarTarefa = document.querySelector('#criar-tarefa');
 const listaTarefas = document.querySelector('#lista-tarefas');
 const textoTarefa = document.querySelector('#texto-tarefa');
 const apagarLista = document.querySelector('#apaga-tudo');
+const removeFinalizados = document.querySelector('#remover-finalizados');
 
+// Adiciona Texto do input na Lista de Tarefas
 const adicionaListaDeTarefa = () => {
   criarTarefa.addEventListener('click', () => {
     const adicionaTarefa = document.createElement('li');
@@ -18,15 +20,16 @@ const adicionaListaDeTarefa = () => {
   });
 };
 
+// Destaca item da lista de tarefas clicado
 const marcaItensDaLista = () => {
   listaTarefas.addEventListener('click', (event) => {
     const itemDaLista = document.querySelectorAll('.tarefa');
-
+    const evento = event.target;
     for (let item = 0; item < itemDaLista.length; item += 1) {
       itemDaLista[item].style.background = 'white';
     }
 
-    event.target.style.background = 'gray';
+    evento.style.background = 'gray';
   });
 
   listaTarefas.addEventListener('dblclick', (event) => {
@@ -34,9 +37,20 @@ const marcaItensDaLista = () => {
   });
 };
 
+// Limpa lista de tarefas removendo todos os itens
 const apagaListaDeTarefas = () => {
   apagarLista.addEventListener('click', () => {
     listaTarefas.innerHTML = '';
+  });
+};
+
+// Remove apenas itens riscados da lista de tarefas
+const apagaFinalizados = () => {
+  removeFinalizados.addEventListener('click', () => {
+    const completed = document.querySelectorAll('.completed');
+    for (let key = 0; key < completed.length; key += 1) {
+      completed[key].remove();
+    }
   });
 };
 
@@ -44,4 +58,5 @@ window.onload = () => {
   adicionaListaDeTarefa();
   marcaItensDaLista();
   apagaListaDeTarefas();
+  apagaFinalizados();
 };
